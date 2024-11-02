6982f4c9b950f442680a48e8ddf3a1986239806b
Ricardo Lima <ricardolimasouza27@gmail.com>
2024-11-01 22:27:14 -0300

    limpeza na coluna cartao_bandeira, seleção de colunas no join

--- transform.py
+++ transform.py
@@ -5,6 +5,7 @@ from pyspark.sql.types import IntegerType, StringType
 
 def df_transform(df,df1):
 
+    ## TRANSFORM COMPRAS
     select_compras = (
         df.select(F.col("id"),
                   F.col("cartao_data_expiracao"),
@@ -13,7 +14,20 @@ def df_transform(df,df1):
                   F.col("cd_livro"),
                   F.col("cd_cliente"))
     )
+    # group_by = (
+    #     select_compras
+    #     .groupby("cartao_bandeira").count()
+    # )
 
+    transform_compras = (
+        select_compras
+        .withColumn("cartao_bandeira", F.regexp_replace(F.col("cartao_bandeira"), "[^a-zA-Z]", " "))
+        .withColumn("cartao_bandeira", F.split(F.col("cartao_bandeira"), "    ").getItem(0))
+        .withColumn("cartao_bandeira", F.split(F.col("cartao_bandeira"), "   ").getItem(0))
+    )
+
+
+    ## TRANSFORM CLIENTE
     transform_name_cliente = (
         df1
         .withColumn("name", F.when(F.col("name").rlike(r"^(Sra\.|Sr\.|Srta\.|Dr)"), F.expr("substring(name, 6, length(name))"))
@@ -44,12 +58,25 @@ def df_transform(df,df1):
                    F.col("estado"))
     )
 
+
+    ## JOIN 
     join_cliente_compras = (
         cliente_transform.alias("cl")
-        .join(select_compras, select_compras.cd_cliente == cliente_transform.id, how= "inner")
+        .join(transform_compras.alias("cp"), F.col("cp.cd_cliente") == F.col("cl.id"), how="inner")
     )
+
+
     teste = (
-        join_cliente_compras.select(F.col("cp.id"))
+        join_cliente_compras.select(F.col("cl.id").alias("id_clientes"),  
+                                    F.col("cl.name"), 
+                                    F.col("cl.idade"), 
+                                    F.col("cl.estado"),
+                                    F.col("cp.id").alias("id_compras"),
+                                    F.col("cp.cartao_bandeira"),
+                                    F.col("cp.data"),
+                                    F.col("cp.cd_livro")
+        )
     )
-    return  select_compras, teste
+    
+    return  transform_compras, teste
 
