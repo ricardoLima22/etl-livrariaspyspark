3689d38910124d189bb15bb97352cd47e7664e1a
Ricardo Lima <ricardolimasouza27@gmail.com>
2024-11-03 20:53:11 -0300

    importação dos dados dos autores

--- main.py
+++ main.py
@@ -12,16 +12,21 @@ def main():
     compras = spark.read.parquet(r".\docs\COMPRAS.parquet", header = True, inferSchema = True, sep = "|").alias("cp")
     clientes = spark.read.parquet(r".\docs\CLIENTES.parquet" ,header = True, inferSchema = True, sep = "|").alias("cl")
     livros = spark.read.parquet(r".\docs\LIVROS.parquet" ,header = True, inferSchema = True, sep = "|").alias("lv")
+    autores = spark.read.parquet(r".\docs\AUTORES.parquet", header = True, inferSchema = True, sep = "|").alias("at")
 
-
-    result_compras, result_clientes = df_transform(df=compras,df1=clientes)
+    result_compras, result_clientes, result_livros, result_autores = df_transform(df=compras,df1=clientes,df2=livros,df3=autores)
 
     result_compras.show(20,False)
     result_clientes.show(20,False)
+    result_livros.show(20,False)
+
+    # autores.show(20,False)
+    result_autores.write.format("console").save()
+
 
     # result_compras.write.format("parquet").mode("overwrite").save("D:\compras")
     # result_clientes.write.format("parquet").mode("overwrite").save("D:\clientes")
-
+    result_autores.write.format("parquet").mode("overwrite").save(r"D:\table_joins")
     
     spark.stop()
 if __name__ == "__main__":
