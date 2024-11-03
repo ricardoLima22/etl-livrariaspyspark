e14b758c929d492c7f5d6d72c98549c6408399c1
Ricardo Lima <ricardolimasouza27@gmail.com>
2024-11-03 20:55:23 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,12 +1,18 @@
 from pyspark.sql import SparkSession
-
+import pyspark.sql.functions as F
 # Inicializando a sessão Spark
 spark = SparkSession.builder.appName("Visualizar Parquet").getOrCreate()
 
 # Lendo o arquivo Parquet
 df = spark.read.parquet(r"D:\clientes")
 df1 = spark.read.parquet(r"D:\compras")
+df2 = spark.read.parquet(r"D:\table_joins")
 
 # Exibindo as primeiras linhas
 df.show(truncate=False)
 df1.show(truncate=False)
+df2.show(truncate=False)
+
+teste = df2.where(F.col("estado").isin("ES"))
+
+teste.write.format("console").save()
